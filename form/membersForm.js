baa42701389e13d117c81d8604e3e2d551ef1426
dahee <hdahee.9@gmail.com>
2021-07-24 17:19:20 +0900

    form태그 실습

---
+++ form/membersForm.js
@@ -0,0 +1,50 @@
+
+// const members = [];
+const membersGet = sessionStorage.getItem('members');
+const membersLogical = membersGet || '[]';
+const members = JSON.parse(membersLogical);
+
+const membersSet = function(){
+  const membersSet = JSON.stringify(members);
+  sessionStorage.setItem('members', membersSet);
+};
+const membersCreate = function(member) {
+  members.push(member);
+  membersSet();
+  return members;
+};
+
+const membersRead = function() {
+  for (let index in members) {
+    document.writeln(members[index]);
+  }
+  return members;
+};
+
+const membersDelete = function(index) {
+  members.splice(index, 1);
+  membersSet();
+ 
+  return members;
+};
+
+const membersUpdate = function(index, member) {
+  members[index] = member;
+  membersSet();
+  return members;
+};
+
+const membersSubmit = function(form) {
+  const inputTextObject = form['input-text'];
+  try {
+    const evalReturn = JSON.parse(inputTextObject.value);
+    console.log(evalReturn);
+  } catch(error) {
+    console.error(error);
+    alert(error);
+    return false;
+  }
+}
+
+
+membersRead();
