0cb4292f554d6216221a5f4d894cf1ca088841d7
dahee <hdahee.9@gmail.com>
2021-08-07 15:09:47 +0900

    homework6

---
+++ homework/homework6-2.js
@@ -0,0 +1,7 @@
+const copy = function() {
+  console.log('copy');
+  // 여기에 프로그램을 작성 해주세요.
+  document.getElementById('input-copy1').value = document.getElementById('input-original').value 
+  document.getElementById('input-copy2').value = document.getElementById('input-original').value 
+
+}
