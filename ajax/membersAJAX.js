59620cdd6a0e8e18ad8560e39ce4fd79bd52c7a6
dahee <hdahee.9@gmail.com>
2021-08-21 12:18:22 +0900

    Backend와 통신 Read

--- ajax/membersAJAX.js
+++ ajax/membersAJAX.js
@@ -35,24 +35,45 @@ const membersCreate = function(form) {
 
 
 const membersRead = function() {
-  const tagDivParent = document.getElementById('tag-div-parent');
-  const tagDivChild = document.getElementById('tag-div-child');
-  tagDivParent.innerHTML = '';
-  for (let index in members) {
-    const newDivChild = tagDivChild.cloneNode(true);
-    tagDivParent.appendChild(newDivChild);
-    const membersNameObject = document.getElementsByName('members-name')[index];
-    const membersAgeObject = document.getElementsByName('members-age')[index];
-    const membersUpdateObject = document.getElementsByName('members-update')[index];
-    const membersDeleteObject = document.getElementsByName('members-delete')[index];
-    membersNameObject.value = members[index].name;
-    membersAgeObject.value = members[index].age;
-    membersUpdateObject.index = index;
-    membersDeleteObject.index = index;
-  }
-  console.log('Readed', members);
-  return members;
+  const successFunction = function(xhrObject) {
+    const membersLogical = JSON.parse(xhrObject.responseText);
+    const members = membersLogical.members;
+    const tagDivParent = document.getElementById('tag-div-parent');
+    const tagDivChild = document.getElementById('tag-div-child');
+    tagDivParent.innerHTML = '';
+    for (let index in members) {
+      const newDivChild = tagDivChild.cloneNode(true);
+      tagDivParent.appendChild(newDivChild);
+      const membersNameObject = document.getElementsByName('members-name')[index];
+      const membersAgeObject = document.getElementsByName('members-age')[index];
+      const membersUpdateObject = document.getElementsByName('members-update')[index];
+      const membersDeleteObject = document.getElementsByName('members-delete')[index];
+      membersNameObject.value = members[index].name;
+      membersAgeObject.value = members[index].age;
+      membersUpdateObject.index = index;
+      membersDeleteObject.index = index;
+    }
+    console.log('Readed', members);
+  };
+  const xhrObject = new XMLHttpRequest();
+  xhrObject.onreadystatechange = function () {
+    if (xhrObject.readyState !== 4) return;
+    if (xhrObject.status === 200) {
+      successFunction(xhrObject);
+    } else {
+      const error = {
+        status: xhrObject.status,
+        statusText: xhrObject.statusText,
+        responseText: xhrObject.responseText
+      }
+      console.error(error);
+    }
+  };
+  xhrObject.open('GET', 'http://localhost:3100/api/v1/members');
+  xhrObject.setRequestHeader('Content-Type', 'application/json');
+  xhrObject.send();
 };
+
 
 const membersDelete = function(index) {
   members.splice(index, 1);
