696663200c87bd3259efb3b761832f5866183f13
dahee <hdahee.9@gmail.com>
2021-08-21 11:54:12 +0900

    Backend와 통신 Create

---
+++ ajax/membersAJAX.js
@@ -0,0 +1,79 @@
+const membersGet = sessionStorage.getItem('members');
+const membersLogical = membersGet || '[]';
+const members = JSON.parse(membersLogical);
+
+const membersCreate = function(form) {
+  const memberNameObject = form['member-name'];
+  const memberAgeObject = form['member-age'];
+  const member = {
+    name: memberNameObject.value,
+    age: memberAgeObject.value
+  };
+  const successFunction = function() {
+    memberNameObject.value = '';
+    memberAgeObject.value = '';
+    membersRead();
+  }
+  const xhrObject = new XMLHttpRequest();
+  xhrObject.onreadystatechange = function() {
+    if (xhrObject.readyState !== 4) return;
+    if (xhrObject.status === 200) {
+      successFunction();
+    } else {
+      const error = {
+        status: xhrObject.status,
+        statusText: xhrObject.statusText,
+        responseText: xhrObject.responseText
+      }
+      console.error(error);
+    }
+  };
+  xhrObject.open('POST', 'http://localhost:3100/api/v1/members');
+  xhrObject.setRequestHeader('Content-Type', 'application/json');
+  xhrObject.send(JSON.stringify(member));
+};
+
+
+const membersRead = function() {
+  const tagDivParent = document.getElementById('tag-div-parent');
+  const tagDivChild = document.getElementById('tag-div-child');
+  tagDivParent.innerHTML = '';
+  for (let index in members) {
+    const newDivChild = tagDivChild.cloneNode(true);
+    tagDivParent.appendChild(newDivChild);
+    const membersNameObject = document.getElementsByName('members-name')[index];
+    const membersAgeObject = document.getElementsByName('members-age')[index];
+    const membersUpdateObject = document.getElementsByName('members-update')[index];
+    const membersDeleteObject = document.getElementsByName('members-delete')[index];
+    membersNameObject.value = members[index].name;
+    membersAgeObject.value = members[index].age;
+    membersUpdateObject.index = index;
+    membersDeleteObject.index = index;
+  }
+  console.log('Readed', members);
+  return members;
+};
+
+const membersDelete = function(index) {
+  members.splice(index, 1);
+  membersSet();
+  return membersRead();
+};
+
+const membersUpdate = function(index) {
+  const name = document.getElementsByName('members-name')[index].value;
+  const age = document.getElementsByName('members-age')[index].value;
+  members[index] = {
+    name: name,
+    age: age
+  };
+  membersSet();
+  return membersRead();
+};
+
+const membersSet = function() {
+  const membersSet = JSON.stringify(members);
+  sessionStorage.setItem('members', membersSet);
+};
+
+membersRead();
